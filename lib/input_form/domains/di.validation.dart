7a6acd75485395b3c895d3b342e2e129e319500b
sounniroth <nirothsoun@gmail.com>
2025-06-17 17:43:35 +0700

    - LocaleName no get from InputConfig

--- lib/input_form/domains/di.validation.dart
+++ lib/input_form/domains/di.validation.dart
@@ -1,9 +1,4 @@
-enum Locale {en, km;
- static Locale fromString(String value){
-   if (value == km.name) return km;
-   return en;
- }
-}
+import 'package:input_controls/utils/config.dart';
 
 /// Each element has it own validate message
 enum Required {text, select;
@@ -19,7 +14,7 @@ enum Required {text, select;
 class Validation {
   Validation._();
   static final instance = Validation._();
-  
+
   final languages = ValidationMsg.instance;
 
   /// Return null if value is valid
@@ -28,7 +23,7 @@ class Validation {
   })
   {
     if (rules.require case final req?) return require(fieldLabel, value, req);
-    
+
     if (value != null) {
       value = value.trim();
 
@@ -40,7 +35,7 @@ class Validation {
       if (rules.max_length case final lenght?) {
         return max_length(fieldLabel, value, lenght);
       }
-      
+
       if (rules.exact_length case final lenght?) {
         return exact_length(fieldLabel, value, lenght);
       }
@@ -73,7 +68,7 @@ class Validation {
 
       if (rules.birth_year_format case final birthYearFormat?) return birth_year_format(fieldLabel, value, birthYearFormat);
     }
-    
+
     return null;
   }
 
@@ -81,7 +76,7 @@ class Validation {
     if (value == null || value.isEmpty) {
       return req.message(fieldLabel);
     }
-    
+
     return null;
   }
 
@@ -96,7 +91,7 @@ class Validation {
     if (value.isNotEmpty && value.length > length) {
       return languages.maxLength(fieldLabel, length);
     }
-    
+
     return null;
   }
 
@@ -112,7 +107,7 @@ class Validation {
     if (double.tryParse(value) case final val? when val >= lessThan) {
       return languages.lessThan(fieldLabel, lessThan);
     }
-    
+
     return null;
   }
 
@@ -128,7 +123,7 @@ class Validation {
     if (double.tryParse(value) case final val? when val <= greaterThan) {
       return languages.greaterThan(fieldLabel, greaterThan);
     }
-    
+
     return null;
   }
 
@@ -136,7 +131,7 @@ class Validation {
     if (double.tryParse(value) case final val? when val < greaterThanEqualTo) {
       return languages.greaterThanEqualTo(fieldLabel, greaterThanEqualTo);
     }
-    
+
     return null;
   }
 
@@ -144,12 +139,12 @@ class Validation {
     if (value.isNotEmpty) {
       const pattern = r'^([0-9])';
       final regex = RegExp(pattern);
-      
+
       if (!regex.hasMatch(value)) {
         return languages.numeric(fieldLabel);
       }
     }
-    
+
     return null;
   }
 
@@ -157,12 +152,12 @@ class Validation {
     if (value.isNotEmpty) {
       const pattern = r'^0[1-9]{1}[0-9]{7,8}$';
       final regex = RegExp(pattern.toString());
-      
+
       if (!regex.hasMatch(value) || value.length > 15) {
         return languages.invalid(fieldLabel);
       }
     }
-    
+
     return null;
   }
 
@@ -174,18 +169,18 @@ class Validation {
         return languages.invalid(fieldLabel);
       }
     }
-    
+
     return null;
   }
 
   String? email_format (String fieldLabel, String value) {
     const pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     final regex = RegExp(pattern.toString());
-    
+
     if (!regex.hasMatch(value)) {
       return languages.invalid(fieldLabel);
     }
-    
+
     return null;
   }
 
@@ -195,43 +190,42 @@ class Validation {
     if (birthYear != null && birthYear >= providedYear){
       return languages.invalidBirthYear(fieldLabel);
     }
-    
+
     return null;
   }
 }
 
 class ValidationMsg {
-  Locale locale = Locale.en;
-
   ValidationMsg._();
   static final instance = ValidationMsg._();
 
   String maxLength (String label, Object param) => get({
-    Locale.en:'The “$label” field cannot exceed $param characters in length.',
-    Locale.km:'“$label” មិនអាចលើសពី $param តួអក្សរដែលមានប្រវែង។',
+    LocaleName.en:'The “$label” field cannot exceed $param characters in length.',
+    LocaleName.km:'“$label” មិនអាចលើសពី $param តួអក្សរដែលមានប្រវែង។',
   });
 
   String minLength (String label, Object param) => get({
-    Locale.en:'The “$label” field must be at least $param characters in length.',
-    Locale.km:'“$label” ត្រូវតែមានយ៉ាងហោចណាស់ $param តួអក្សរ។',
+    LocaleName.en:'The “$label” field must be at least $param characters in length.',
+    LocaleName.km:'“$label” ត្រូវតែមានយ៉ាងហោចណាស់ $param តួអក្សរ។',
   });
 
-  String invalid(String label) => get({Locale.en:"The “$label” is invalid.",Locale.km:"“$label” មិនត្រឹមត្រូវទេ។"});
-  String required(String label) => get({Locale.en:"Please enter the “$label”.",Locale.km:"សូមបញ្ចូល “$label”។"});
-  String selectRequired(String label) => get({Locale.en:"Please select any “$label”.",Locale.km:"សូមជ្រើសរើស “$label” ណាមួយ។"});
-  String numeric(String label) => get({Locale.en:"The “$label” field must contain only numbers.", Locale.km:"“$label” ត្រូវមានតែលេខប៉ុណ្ណោះ។"});
-  String invalidBirthYear(String label) => get({Locale.en:"The “$label” must be smaller than current year.",Locale.km:"“$label” ត្រូវតែតូចជាងឆ្នាំបច្ចុប្បន្ន។"});
+  String invalid(String label) => get({LocaleName.en:"The “$label” is invalid.",LocaleName.km:"“$label” មិនត្រឹមត្រូវទេ។"});
+  String required(String label) => get({LocaleName.en:"Please enter the “$label”.",LocaleName.km:"សូមបញ្ចូល “$label”។"});
+  String selectRequired(String label) => get({LocaleName.en:"Please select any “$label”.",LocaleName.km:"សូមជ្រើសរើស “$label” ណាមួយ។"});
+  String numeric(String label) => get({LocaleName.en:"The “$label” field must contain only numbers.", LocaleName.km:"“$label” ត្រូវមានតែលេខប៉ុណ្ណោះ។"});
+  String invalidBirthYear(String label) => get({LocaleName.en:"The “$label” must be smaller than current year.",LocaleName.km:"“$label” ត្រូវតែតូចជាងឆ្នាំបច្ចុប្បន្ន។"});
 
-  String exactLength(String label, Object param) => get({Locale.en:"The “$label” field must be equal $param characters in length.",Locale.km:"“$label” ត្រូវតែស្មើ $param តួអក្សរដែលមានប្រវែង។"});
-  String lessThan(String label, Object param) => get({Locale.en:"The “$label” field must be less than $param.",Locale.km:"“$label” ត្រូវតែតិចជាង $param។"});
-  String lessThanEqualTo(String label, Object param) => get({Locale.en:"The “$label” field must be less than or equal $param.",Locale.km:"“$label” ត្រូវតែតិចជាង ឬស្មើ $param។"});
-  String greaterThan(String label, Object param) => get({Locale.en:"The “$label” field must be greater than $param.",Locale.km:"“$label” ត្រូវតែធំជាង $param។"});
-  String greaterThanEqualTo(String label, Object param) => get({Locale.en:"The “$label” field greater than or equal $param.",Locale.km:"“$label” ត្រូវតែធំជាង ឬស្មើ $param។"});
+  String exactLength(String label, Object param) => get({LocaleName.en:"The “$label” field must be equal $param characters in length.",LocaleName.km:"“$label” ត្រូវតែស្មើ $param តួអក្សរដែលមានប្រវែង។"});
+  String lessThan(String label, Object param) => get({LocaleName.en:"The “$label” field must be less than $param.",LocaleName.km:"“$label” ត្រូវតែតិចជាង $param។"});
+  String lessThanEqualTo(String label, Object param) => get({LocaleName.en:"The “$label” field must be less than or equal $param.",LocaleName.km:"“$label” ត្រូវតែតិចជាង ឬស្មើ $param។"});
+  String greaterThan(String label, Object param) => get({LocaleName.en:"The “$label” field must be greater than $param.",LocaleName.km:"“$label” ត្រូវតែធំជាង $param។"});
+  String greaterThanEqualTo(String label, Object param) => get({LocaleName.en:"The “$label” field greater than or equal $param.",LocaleName.km:"“$label” ត្រូវតែធំជាង ឬស្មើ $param។"});
 
-  String get priceGreaterThanEqualTo => get({Locale.en:"Please enter the valid price.",Locale.km:"សូមបញ្ចូលតម្លៃអោយបានត្រឹមត្រូវ។"});
+  String get priceGreaterThanEqualTo => get({LocaleName.en:"Please enter the valid price.",LocaleName.km:"សូមបញ្ចូលតម្លៃអោយបានត្រឹមត្រូវ។"});
 
-  String get(Map<Locale, String> languages){
-    if (languages[locale] case final result?) return result;
+  String get(Map<LocaleName, String> languages){
+    final config = InputConfig.instance;
+    if (languages[config.locale] case final result?) return result;
     return languages.values.first;
   }
 }
